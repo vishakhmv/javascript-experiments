5b6b0cf62100abdaaabdb9b0d6ade0c893068696
Vishakh M V <vishakhmv667@gmail.com>
2025-12-06 22:31:07 +0530

    remove some comment lines from guessgame.js

--- guessgame/guessgame.js
+++ guessgame/guessgame.js
@@ -1,34 +1,10 @@
-// let num=24;
-// let userNumber=Number(prompt("Enter the number"));
-// while(true)
-// {
-//     if(userNumber===num)
-//     {
-//         alert("Your Guess was correct");
-//         break;
-//     }
-//     else
-//     {
-//         userNumber=Number(prompt(("Your guess was wrong, Try again")));
-//     }
-// }
-
-
-
-
-
-
-let num=24;
-let userNumber=prompt("Enter the number");
-while(true)
-{
-    if(userNumber==num)
-    {
-        alert("Your Guess was correct");
-        break;
-    }
-    else
-    {
-        userNumber=prompt(("Your guess was wrong, Try again"));
-    }
+let num = 24;
+let userNumber = prompt("Enter the number");
+while (true) {
+  if (userNumber == num) {
+    alert("Your Guess was correct");
+    break;
+  } else {
+    userNumber = prompt("Your guess was wrong, Try again");
+  }
 }
