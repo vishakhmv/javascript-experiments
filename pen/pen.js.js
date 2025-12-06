a5b2d9b991f1f5aa0572a332aebbc1ba842a10d3
Vishakh M V <vishakhmv667@gmail.com>
2025-12-06 22:21:20 +0530

    add pen folder which have pen.js and pen.html files

---
+++ pen/pen.js.js
@@ -0,0 +1,9 @@
+const pen={
+    name  :"Parker Jotter Standard CT Ball Pen (Black)",
+    ratting : 4,
+    dealOfTheDya:true,
+    offerInPercentage:5,
+    finalPrice:270,
+};
+
+console.log(pen)
