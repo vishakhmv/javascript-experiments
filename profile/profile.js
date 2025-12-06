2bddb8c367d7c523446042792504673899e57c76
Vishakh M V <vishakhmv667@gmail.com>
2025-12-06 22:22:31 +0530

    add profile folder which have profile.js and profile.html files

---
+++ profile/profile.js
@@ -0,0 +1,10 @@
+const profile={
+    username:"@shradhakhapra",
+    folowers:569000,
+    folowing:4,
+    isFollowing:false,
+};
+
+console.log(profile);
+console.log(typeof profile);
+console.log(typeof profile["username"]);
